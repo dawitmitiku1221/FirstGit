0b8577753683555a10e215b6627aba0f91e31a9b
Berhanu Kebito <58462920+brhan-kbt@users.noreply.github.com>
2022-03-29 10:26:45 +0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    //This is edited 
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
